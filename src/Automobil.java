7b95556ae49d5755be2c65e4b9756346d02d4ca3
itafra <ivanatafra16@gmail.com>
2025-04-28 20:02:57 +0200

    Add Automobil class, constructor, getter, setter, prikaziPodatke method

---
+++ src/Automobil.java
@@ -0,0 +1,26 @@
+public class Automobil extends Vozilo {
+    private int brojVrata;
+
+    public Automobil(String marka, String registarskiBroj, int godinaProizvodnje, int brojVrata) {
+        super(marka, registarskiBroj, godinaProizvodnje);
+        this.brojVrata = brojVrata;
+    }
+    public class Main {
+        public static void main(String[] args) {
+            Automobil automobil = new Automobil("Toyota", "ZG1234AB", 2020, 4);
+            automobil.prikaziPodatke();
+        }
+    }
+
+    public int getBrojVrata() {
+    return brojVrata;
+    }
+    public void setBrojVrata(int brojVrata) {
+        this.brojVrata = brojVrata;
+    }
+    @Override
+    public void prikaziPodatke() {
+        super.prikaziPodatke();
+        System.out.println("Broj vrata automobila je: " + brojVrata);
+    }
+}
